7e47f6da5e9ecdb9db9c6a3a8ee6a7afe5d61d07
seung7361 <seung7361@naver.com>
2022-02-06 00:25:36 +0900

    topnav-search bar & new icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,6 +16,22 @@ function Header() {
                 <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
                     <FiInstagram />
                 </div>
+
+                {/* Middle */}
+                <div className="relative mt-1 p-3 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <AiOutlineSearch className="text-gray-300" />
+                    </div>
+                    <input className="bg-gray-50 justify-between block w-full pl-10 sm:text-sm border-gray-300
+                                        focus:ring-black focus:border-black rounded-md
+                    " type="text" placeholder="Search..."/>
+                </div>
+
+                {/* Right */}
+                <div className="flex items-center justify-end space-x-4">
+                    <AiOutlineHome className="h-10 w-10 cursor-pointer" />
+                    <AiOutlineMenu className="h-6 md:hidden cursor-pointer" />
+                </div>                
             </div>
         </div>
     );
