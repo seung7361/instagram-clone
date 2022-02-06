095575140bed0b293752914c7285465fd1539f59
seung7361 <seung7361@naver.com>
2022-02-06 23:40:45 +0900

    Header done (almost)

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,14 +29,33 @@ function Header() {
                     </svg>
 
                     {/* paper airplane icon */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="navBtn" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-                    </svg>
+                    <div className="relative navBtn">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="navBtn" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                        </svg>
+                        <div className="absolute -top-2 -right-2 text-xs w-5 h-5
+                        bg-red-500 rounded-full flex items-center justify-center
+                        text-white animate-bounce">3</div>
+                    </div>
 
                     {/* Plus */}
                     <svg xmlns="http://www.w3.org/2000/svg" className="navBtn" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                     </svg>
+
+                    {/* fire icon */}
+                    <svg xmlns="http://www.w3.org/2000/svg" className="navBtn" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                    </svg>
+
+                    {/* Heart icon */}
+                    <svg xmlns="http://www.w3.org/2000/svg" className="navBtn hover:fill-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+
+                    {/* User Image */}
+                    <img src="/avatar.png" alt="profile pic" className="hover:scale-125 transition-all duration-150 h-10 rounded-full cursor-pointer" />
                 </div>
             </div>
         </div>
