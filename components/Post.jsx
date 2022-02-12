ec0223597bbecb21132614827913116942b3ea14
seung7361 <seung7361@naver.com>
2022-02-12 23:51:06 +0900

    Added Posts

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,6 +1,47 @@
-function Post() {
+import DotsHorizontal from "@heroicons/react/outline/DotsHorizontalIcon";
+import HeartIcon from "@heroicons/react/outline/HeartIcon";
+import HeartIconFilled from "@heroicons/react/solid/HeartIcon";
+import Comment from "@heroicons/react/outline/ChatIcon";
+import PaperAirplane from "@heroicons/react/outline/PaperAirplaneIcon";
+import BookmarkIcon from "@heroicons/react/outline/BookmarkIcon";
+
+function Post({ id, username, userimg, img, caption }) {
+    console.log(username, userimg);
     return (
-        <div></div>
+        <div>
+            {/* Header */}
+            <div className="flex items-center p-5 bg-white mt-7 border rounded-sm">
+                <img src={userimg}
+                className="rounded-full h-12 w-12 object-contain p-1 border mr-3"
+                alt="profile pic" />
+                <p className="flex-1 font-bold">{username}</p>
+                <DotsHorizontal className="h-5 cursor-pointer" />
+            </div>
+
+            {/* Image */}
+            <img src={img} alt="source image" className="object-cover w-full" />
+
+            {/* buttons */}
+            <div className="flex justify-between px-4 pt-4">
+                <div className="flex space-x-4 flex-1">
+                    <HeartIcon className="btn" />
+                    <Comment className="btn" />
+                    <PaperAirplane className="btn" />
+                </div>
+
+                <BookmarkIcon className="btn" />
+            </div>
+
+            {/* caption */}
+            <p className="p-5 truncate">
+                <span className="font-bold mr-1">{username}</span>
+                {caption}
+            </p>
+
+            {/* comments */}
+
+            {/* input box */}
+        </div>
     );
 }
 
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,13 +1,29 @@
 import Post from "./Post.jsx";
 
-const DUMMY_DATA = [
-    
-]
+const posts = [
+    {
+        id: '123',
+        username: 'seung7361',
+        userimg: '/avatar.png',
+        img: '/example image.jpeg',
+        caption: 'This is dope!'
+    },
+    {
+        id: '123',
+        username: 'seung7361',
+        userimg: '/avatar.png',
+        img: '/example image.jpeg',
+        caption: 'This is dope!'
+    },
+];
 
 function Posts() {
     return (
         <div>
-            <Post />
+            {posts.map(post => (
+                <Post key={post.id} id={post.id} username={post.username}
+                userimg={post.userimg} img={post.img} caption={post.caption} />
+            ))}
         </div>
     );
 }
