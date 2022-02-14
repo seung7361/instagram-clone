3e43c6d18cf72a45e662ea4b7d90570fad47ca5a
seung7361 <seung7361@naver.com>
2022-02-14 23:14:05 +0900

    Added some stuffs to signin page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,15 +1,25 @@
 import { getProviders, signIn } from "next-auth/react";
+import Header from "../../components/Header";
 
 function signin({ providers }) {
     return (
         <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                <button onClick={() => signIn(provider.id)}>
-                    Sign in with {provider.name}
-                </button>
+            <Header />
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-20 px-14 text-center">
+                <img className="w-80" src="/creator.jpeg" alt="instagram logo" />
+                <p className="font-xs italic">Instagram clone created by SeungLabFx</p>
+                <p className="font-xs text-gray-300">(Much like 'followed tutorial by' but...)</p>
+                <div className="mt-40">
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                        <button className="p-3 bg-blue-500 rounded-lg text-white"
+                        onClick={() => signIn(provider.id)}>
+                            Sign in with {provider.name}
+                        </button>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </>
     );
 }
